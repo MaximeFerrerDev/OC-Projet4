84843977c4d01ebf3127092733b2bdebbe9c87b7
MaximeFerrerDev <maximeferrer.dev@gmail.com>
2024-02-05 17:48:00 +0100

    Rich results put in another file

---
+++ assets/richresults.js
@@ -0,0 +1,69 @@
+{
+    "@context": "https://schema.org",
+    "@type": "ProfessionalService",
+    "image": [
+      "https://nina-carducci.github.io/assets/images/nina.png"
+     ],
+    "name": "Nina Carducci - Photographie",
+    "address": {
+      "@type": "PostalAddress",
+      "streetAddress": "148 W 51st St",
+      "addressLocality": "New York",
+      "addressRegion": "NY",
+      "postalCode": "10019",
+      "addressCountry": "US"
+    },
+    "review": {
+      "@type": "Review",
+      "reviewRating": {
+        "@type": "Rating",
+        "ratingValue": "4",
+        "bestRating": "5"
+      },
+      "author": {
+        "@type": "Person",
+        "name": "Nina Carducci"
+      }
+    },
+    "geo": {
+      "@type": "GeoCoordinates",
+      "latitude": 40.761293,
+      "longitude": -73.982294
+    },
+    "url": "https://nina-carducci.github.io/assets/images/nina.png",
+    "telephone": "+12122459600",
+    "priceRange": "$$$",
+    "openingHoursSpecification": [
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+          "Monday",
+          "Tuesday"
+        ],
+        "opens": "11:30",
+        "closes": "22:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": [
+          "Wednesday",
+          "Thursday",
+          "Friday"
+        ],
+        "opens": "11:30",
+        "closes": "23:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Saturday",
+        "opens": "16:00",
+        "closes": "23:00"
+      },
+      {
+        "@type": "OpeningHoursSpecification",
+        "dayOfWeek": "Sunday",
+        "opens": "16:00",
+        "closes": "22:00"
+      }
+    ]
+  }
